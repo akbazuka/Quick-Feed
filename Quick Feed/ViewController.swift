1f225dcf7e9b481c98e4eab290d92e391f77d5d7
AK <ak@yoshitsugumamoru-no-macbook-pro.local>
2019-02-12 17:39:59 -1000

    Initial Commit

---
+++ Quick Feed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Feed
+//
+//  Created by AK on 2/12/19.
+//  Copyright © 2019 Kedlena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
